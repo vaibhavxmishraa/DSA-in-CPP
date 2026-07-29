43290ad4133502634ab7a05efe41ecf4e6734844
Vaibhav Mishra <vaibhavmishra983907@gmail.com>
2026-07-29 16:34:41 +0530

    Add examples for long long, short, and double types
    Added examples for long long, short, and double data types with their respective minimum and maximum values.

---
+++ data-type_range.cpp
@@ -0,0 +1,49 @@
+// What is the maximum range to store the data in int
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+  int x = INT_MAX;
+  cout<< x << endl;
+  int y = INT_MIN;
+  cout<< y << endl;
+}
+
+//Long long  data type (long long also stores only integer data types.)
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+  long long x = LLONG_MAX;
+  cout<< x << endl;
+  long long y = LLONG_MIN;
+  cout<< y << endl;
+}
+
+// Short data type (it also stores only integer values)
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    short x = SHRT_MIN, y = SHRT_MAX;
+    cout << x << endl;
+    cout << y << endl;
+    return 0;
+}
+
+// Double data type
+// This also stores decimal values
+//Double vs float
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+  double x = 3.123456789098765;
+  float y = 3.123456789098765;
+cout << x << endl;
+cout<< y << endl;
+}
+
+//
+  
